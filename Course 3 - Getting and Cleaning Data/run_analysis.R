a3afc29ad069c285db91cae863365c1512b21ef4
rgmaier <robert.maier@protonmail.ch>
2020-05-05 21:34:28 +0200

    Added data read functionality

---
+++ Course 3 - Getting and Cleaning Data/run_analysis.R
@@ -0,0 +1,11 @@
+course3 <- function(){
+  
+  delimiter <- rep(16, times=561)
+  trainSetX <- read.fwf("X_train.txt", delimiter)
+  testSetX <- read.fwf("X_test.txt", delimiter)
+  
+  trainSetY <- read.csv("y_train.txt", header=F)
+  testSetY <- read.csv("y_test.txt", header=F)
+  
+  features <- read.csv("features.txt", header=F, sep=" ")
+}
